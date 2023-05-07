e67fe646dee0a5b1d73da4d273c2796e3cfd8a21
Wesly Backers <45541801+WeslyBackers@users.noreply.github.com>
2023-05-07 15:31:29 +0200

    Initial navigation setup

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,62 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import  { Container } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+//import { Test } from './Buttons.styles';
+
+class Buttons extends PureComponent { 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentWillMount = () => {
+    console.log('Buttons will mount');
+  }
+
+  componentDidMount = () => {
+    console.log('Buttons mounted');
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    console.log('Buttons will receive props', nextProps);
+  }
+
+  componentWillUpdate = (nextProps, nextState) => {
+    console.log('Buttons will update', nextProps, nextState);
+  }
+
+  componentDidUpdate = () => {
+    console.log('Buttons did update');
+  }
+
+  componentWillUnmount = () => {
+    console.log('Buttons will unmount');
+  }
+
+  render () {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return (
+      <div className="ButtonsWrapper">
+        <Container>
+          <Button>button 1</Button>
+        </Container>
+      </div>
+    );
+  }
+}
+
+Buttons.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Buttons.defaultProps = {
+  // bla: 'test',
+};
+
+export default Buttons;
