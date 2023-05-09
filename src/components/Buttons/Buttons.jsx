fc131f2b3b8898d748ab74e5579c36bd04f193d8
Wesly Backers <45541801+WeslyBackers@users.noreply.github.com>
2023-05-09 22:20:11 +0200

    toolbuttons descriptions

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -41,6 +41,18 @@ class Buttons extends PureComponent {
     if (this.state.hasError) {
       return <h1>Something went wrong.</h1>;
     }
+
+    var buttonslist = [    
+    {'id':0,'caption':'JSON','function':this.openJson},
+    {'id':1,'caption':'GML','function':this.openGml},
+    {'id':2,'caption':'Inleiding','function':this.openInleiding,'data_bs_toggle':'collapse','data_bs_target':'#inleiding','aria_controls':'collapseExample','aria_expanded':'false'},
+    {'id':3,'caption':'Kaart','function':this.openChart,'data_bs_toggle':'collapse','data_bs_target':'#chart','aria_controls':'collapseExample','aria_expanded':'false'},
+    {'id':4,'caption':'Print','function':this.printBook.bind(this)},
+    {'id':5,'caption':'Info data','function':this.openInfo.bind,'data_bs_toggle':'collapse','data_bs_target':'#info','aria_controls':'collapseExample','aria_expanded':'true'},
+    {'id':6,'caption':'Werkwijze','function':this.openWorkflow},
+    {'id':7,'caption':'Geschiedenis','function':this.openHistory}
+  ]
+
     return (
       <div className="ButtonsWrapper col-4">
         <Container>
